d6a17e877381c3a61e656619e1fd6198468c3be9
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-10-18 22:18:10 -0400

    add media queries

--- js/index.js
+++ js/index.js
@@ -1,110 +1,53 @@
 (function jukebox() {
-var audio = document.getElementById('audio');
-var play = document.getElementById('play');
-var pause = document.getElementById('pause');
-var stop = document.getElementById('stop');
-var next = document.getElementById('next');
-var hitchhiker = document.getElementById('btn3');
-var strength = document.getElementById('btn4');
-var powderfinger = document.getElementById('btn5');
-var duration = document.getElementById('duration');
+  var audio = document.getElementById('audio');
+  var play = document.getElementById('play');
+  var pause = document.getElementById('pause');
+  var stop = document.getElementById('stop');
+  var hitchhiker = document.getElementById('btn3');
+  var strength = document.getElementById('btn4');
+  var powderfinger = document.getElementById('btn5');
 
-document.getElementById('songtext').innerHTML = 'Hitchhiker By Neil Young'
+  document.getElementById('songtext').innerHTML = 'Hitchhiker By Neil Young'
 
-//var songs = [
- //song1 = [ audio.src = 'audio/hitchhiker.mp3' },
- //song2 = { Title: 'Give Me Strength', Artist: 'Neil Young', src: 'audio/givemestrength.mp3' } ],
- //song3 = { Title: 'Powderfinger',  Artist: 'Neil Young', src: 'audio/powderfinger.mp3' };
+  function playsong() {
+    audio.play();
+  }
 
+  function playsong1() {
 
+    audio.src = 'audio/hitchhiker.mp3';
+    audio.play();
+    document.getElementById('songtext').innerHTML = 'Hitchhiker By Neil Young';
+  };
 
-//var tracks = [];
-//function addTrack() {
-  // songs.forEach(src => tracks.push(track));
- //};
+  function playsong2() {
+    audio.src = 'audio/givemestrength.mp3';
+    audio.play();
+    document.getElementById('songtext').innerHTML = 'Give Me Strength by Neil Young';
+  };
 
-//songtext.innerHTML = songs[0].Title + ' by ' + songs[0].Artist;
+  function playsong3() {
+    audio.src = 'audio/powderfinger.mp3';
+    audio.play();
+    document.getElementById('songtext').innerHTML = 'Powderfinger by Neil Young';
+  };
 
-function playsong() {
-  audio.play();
-}
+  function pausesong() {
+    audio.pause();
 
-function playsong1() {
+  };
 
-  audio.src = 'audio/hitchhiker.mp3';
-  audio.play();
-  document.getElementById('songtext').innerHTML = 'Hitchhiker By Neil Young';
-};
+  function stopsong() {
+    audio.pause();
+    audio.currentTime = 0;
+  };
 
-function playsong2() {
-  audio.src = 'audio/givemestrength.mp3';
-  audio.play();
-  document.getElementById('songtext').innerHTML = 'Give Me Strength by Neil Young';
-};
-
-function playsong3() {
-audio.src = 'audio/powderfinger.mp3';
-  audio.play();
-  document.getElementById('songtext').innerHTML = 'Powderfinger by Neil Young';
-};
-
-function pausesong() {
-  audio.pause();
-
-};
-
-function stopsong() {
- audio.pause();
- audio.currentTime = 0;
-};
-
-function duration() {
-  audio.duration();
-}
-
-//function nextsong() {
- //var value = function() {
-  // songs[currentValue + 1]
- //}
- //audio.pause();
-// }
-
-//if ( audio.src = 'audio/hitchhiker.mp3' ) {
-//  function() {playsong2} } else if ( audio.src = 'audio/givemestrength.mp3' ) {
-//  function() {playsong3} } else if ( audio.src = 'audio/powderfinger.mp3')
-//  { function() {playsong1} };
-//};
-
-
- //audio.src = songs[1].src;
-
-  //var currentNum = (counter + 1);
-    //songs[currentNum].src;
-    //audio.play();
-    //songtext.innerHTML = songs[1].Title + ' by ' + songs[1].Artist;
-
-  //};
- //function (){
-   //for (let i = 0; i < songs.length; i++) {
-   //audio.src( songs[i+1].src );
- //} }
-
- //function hitchhiker() {
-//   audio.pause();
-//   audio.currentTime = 0;
-//   audio.src = "audio/hitchhiker.mp3";
-//   songtext.innerHTML = "Hitchhiker by Neil Young";
-//   audio.play();
-// }
-
-
-play.addEventListener('click', playsong);
-pause.addEventListener('click', pausesong);
-stop.addEventListener('click', stopsong);
-//next.addEventListener('click', nextsong);
-hitchhiker.addEventListener('click', playsong1);
-strength.addEventListener('click', playsong2);
-powderfinger.addEventListener('click', playsong3);
-duration.addEventListener('load', duration);
+  play.addEventListener('click', playsong);
+  pause.addEventListener('click', pausesong);
+  stop.addEventListener('click', stopsong);
+  hitchhiker.addEventListener('click', playsong1);
+  strength.addEventListener('click', playsong2);
+  powderfinger.addEventListener('click', playsong3);
+  duration.addEventListener('load', duration);
 
 })()
